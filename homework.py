1d5118128fc36b6921767205fab7d98b5ae97f61
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-04-27 14:23:26 +0300

    .

--- homework.py
+++ homework.py
@@ -84,7 +84,7 @@ def main():
 
     while True:
         try:
-            new_homework = get_homework_statuses(0)
+            new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(
                     parse_homework_status(new_homework.get('homeworks')[0]),
