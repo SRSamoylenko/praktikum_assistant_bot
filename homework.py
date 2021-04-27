3d5405d5bb91cc7bd32044607b755a033aa83415
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-04-27 14:14:53 +0300

    first commit

--- homework.py
+++ homework.py
@@ -1,5 +1,9 @@
+import logging
 import os
 import time
+from enum import Enum
+from logging.handlers import RotatingFileHandler
+from typing import Dict, NewType
 
 import requests
 import telegram
@@ -7,46 +11,94 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_FORMAT = '%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 
 
-def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+class HomeworkStatus(Enum):
+    APPROVED = 'approved'
+    REJECTED = 'rejected'
+    REVIEWING = 'reviewing'
+
+
+HomeworkData = NewType('HomeworkData', Dict)
+
+Homeworks = NewType('Homeworks', Dict)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = RotatingFileHandler(
+    BASE_DIR + f'/{__name__}.log',
+    maxBytes=50000000,
+    backupCount=5,
+)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter(LOG_FORMAT)
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+
+def parse_homework_status(homework: HomeworkData) -> str:
+    """Проверяет статус работы.
+    Принимает на вход словарь с данными о домашней работе,
+    возвращает строку, сообщающую о статусе работы.
+    """
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+    if HomeworkStatus(status) is HomeworkStatus.REJECTED:
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = ('Ревьюеру всё понравилось, '
+                   'можно приступать к следующему уроку.')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
-def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+def get_homework_statuses(current_timestamp: int) -> Homeworks:
+    """Возвращает словарь c текущей датой и списком домашних работ,
+    статус которых изменился с момента current_timestamp.
+    """
+    params = {'from_date': current_timestamp}
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    homework_statuses = requests.get(
+        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+        params=params,
+        headers=headers,
+    )
     return homework_statuses.json()
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    logger.info('Отправка сообщения')
+    return bot_client.send_message(CHAT_ID, message)
 
 
 def main():
-    # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    logger.debug('Запуск бота')
+    bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
+    current_timestamp = int(time.time())
 
     while True:
         try:
-            new_homework = get_homework_statuses(current_timestamp)
+            new_homework = get_homework_statuses(0)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0]),
+                    bot_client,
+                )
+            current_timestamp = new_homework.get(
+                'current_date',
+                current_timestamp
+            )
+            time.sleep(300)
 
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
+            logger.exception(e)
+            send_message(f'Бот столкнулся с ошибкой: {e}', bot_client)
             time.sleep(5)
 
 
