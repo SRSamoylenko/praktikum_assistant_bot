f52953c6e64ee6d9888cee1328c0d2fa72e19663
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-05-01 22:22:44 +0300

    Учтены комментарии ревьюера.

--- homework.py
+++ homework.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import time
-from enum import Enum
 from logging.handlers import RotatingFileHandler
 from typing import Dict, NewType
 
@@ -11,23 +10,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_FILE_FORMAT = '%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 LOG_CONSOLE_FORMAT = '%(levelname)s, %(message)s, %(name)s'
-
-
-class HomeworkStatus(Enum):
-    APPROVED = 'approved'
-    REJECTED = 'rejected'
-    REVIEWING = 'reviewing'
-
-
-HomeworkData = NewType('HomeworkData', Dict)
-
-Homeworks = NewType('Homeworks', Dict)
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -48,20 +34,52 @@ handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 
+try:
+    PRAKTIKUM_TOKEN = os.environ['PRAKTIKUM_TOKEN']
+    TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+    CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+except KeyError as e:
+    logger.exception(f'Переменная окружения {e} не найдена. '
+                     f'Запуск бота остановлен.')
+
+HOMEWORK_VERDICTS = {
+    'approved': ('У вас проверили работу "{homework_name}"!\n\n'
+                 'Ревьюеру всё понравилось, '
+                 'можно приступать к следующему уроку.'),
+    'rejected': ('У вас проверили работу "{homework_name}"!\n\n'
+                 'К сожалению в работе нашлись ошибки.'),
+    'reviewing': 'Работа {homework_name} взята на проверку.',
+    'error': 'Ответ не содержит информации о статусе работы.',
+}
+TIME_TO_SLEEP = 300
+
+
+HomeworkData = NewType('HomeworkData', Dict)
+
+Homeworks = NewType('Homeworks', Dict)
+
 
 def parse_homework_status(homework: HomeworkData) -> str:
     """Проверяет статус работы.
     Принимает на вход словарь с данными о домашней работе,
     возвращает строку, сообщающую о статусе работы.
     """
-    homework_name = homework.get('homework_name')
-    status = homework.get('status')
-    if HomeworkStatus(status) is HomeworkStatus.REJECTED:
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = ('Ревьюеру всё понравилось, '
-                   'можно приступать к следующему уроку.')
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    homework_name = None
+    status = 'error'
+    result = 'Ошибка: Ответ содержит неизвестный статус'
+
+    try:
+        homework_name = homework['homework_name']
+        status = homework['status']
+    except KeyError as e:
+        logger.exception(f'Ответ не содержит поля {e}.')
+
+    try:
+        result = HOMEWORK_VERDICTS[status].format(homework_name=homework_name)
+    except KeyError as e:
+        logger.exception(f'Ответ содержит неизвестный статус: {e}')
+
+    return result
 
 
 def get_homework_statuses(current_timestamp: int) -> Homeworks:
@@ -70,12 +88,19 @@ def get_homework_statuses(current_timestamp: int) -> Homeworks:
     """
     params = {'from_date': current_timestamp}
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-    homework_statuses = requests.get(
-        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        params=params,
-        headers=headers,
-    )
-    return homework_statuses.json()
+    result = {}
+
+    try:
+        homework_statuses = requests.get(
+            'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+            params=params,
+            headers=headers,
+        )
+        result = homework_statuses.json()
+    except AttributeError as e:
+        logger.exception(f'homework_statuses не имеет аттрибута {e}.')
+
+    return result
 
 
 def send_message(message, bot_client):
@@ -90,17 +115,16 @@ def main():
 
     while True:
         try:
-            new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(
-                    parse_homework_status(new_homework.get('homeworks')[0]),
-                    bot_client,
-                )
-            current_timestamp = new_homework.get(
+            updated_statuses = get_homework_statuses(current_timestamp)
+            if updated_statuses.get('homeworks'):
+                last_homework = updated_statuses.get('homeworks')[0]
+                message = parse_homework_status(last_homework)
+                send_message(message, bot_client)
+            current_timestamp = updated_statuses.get(
                 'current_date',
                 current_timestamp
             )
-            time.sleep(300)
+            time.sleep(TIME_TO_SLEEP)
 
         except Exception as e:
             logger.exception(e)
